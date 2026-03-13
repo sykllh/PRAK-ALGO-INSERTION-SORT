4a404fcbc253086e8ae1a7fab4237f3a9e4ebe8f
sykllh <sykllhidmi@gmail.com>
2026-03-13 15:01:11 +0700

    menambahkan void display

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -49,3 +49,15 @@ void insertionsort(){  //procedure insertion sort
     }
 
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] <<endl;
+    }
+    cout << endl;
+}
