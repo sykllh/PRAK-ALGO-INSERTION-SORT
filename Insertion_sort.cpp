13f050d4dc6a7b403d33130fdd4a0ad2e522dcbf
sykllh <sykllhidmi@gmail.com>
2026-03-13 15:35:59 +0700

    menambahkan cout baris agar rapi

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -17,9 +17,9 @@ void input (){ //procedure input
         }
     }
     cout << endl;
-    cout << "==============================================";
-    cout << "=========Masukkan Element Array===============";
-    cout << "==============================================";
+    cout << "============================================" << endl;
+    cout << "======== Masukkan Element Array ===========" << endl;
+    cout << "============================================" << endl;
 
     for (int i = 0; i < n; i++ ){
         cout << "Data ke-" << (i+1) << ": ";
@@ -69,3 +69,4 @@ int main()
     display();
     system("pause");
 }
+
