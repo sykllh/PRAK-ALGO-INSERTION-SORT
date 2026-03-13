6a9ca6e218dd48ae778ba4ad2f32daf9cf7ffce9
sykllh <sykllhidmi@gmail.com>
2026-03-13 15:00:35 +0700

    menambahkan prosedur insertion

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -26,3 +26,26 @@ void input (){ //procedure input
         cin >> arr[i];
     }
 }
+
+void insertionsort(){  //procedure insertion sort
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i <<": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+
+}
